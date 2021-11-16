7693d61ccfe46b71430e05167a821bf24bfac9be
Daniel Harty <daniel@adaltas.com>
2021-11-16 16:47:02 +0100

    Removing unused/unecessary code

--- setup.sh
+++ setup.sh
@@ -10,9 +10,5 @@ mkdir -p ./logs
 mkdir -p ./files
 mkdir -p ./ansible_roles
 
-# Create log files
-touch ./logs/vagrant.log 
-touch ./logs/tdp.log
-
 # Clone ansible-tdp-roles repository (doesn't fail iof not known host)
 git clone -o StrictHostKeyChecking=no git@github.com:TOSIT-FR/ansible-tdp-roles.git ansible_roles/collections/ansible_collections/tosit/tdp/
