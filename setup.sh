9fb061794a0b3e99c0ff68f1744defddf9028000
leopaul36 <leopaul36@yahoo.fr>
2022-04-19 18:05:16 +0200

    Fixes and setup improvements (#49)
    * fix: assign vms virtualbox host network ips
    
    * fix: reduce vm deploy processes
    
    * setup: download releases refactor
    
    * fix: nested files dir symbolic links
    
    * setup: added solr to releases
    
    * hdfs: add jxm exporter from tdp-collection
    
    * setup: reorder releases and add hbase
    
    * fix:  tdp_user keytab paths
    
    Aligned with tdp-collection-extras user
    refactor from Apr 2022 deploys user
    
    * setup: add zookeeper release
    
    * refactor: removing jmx exporter task
    
    * fix: kinit missing space
    
    Co-authored-by: Daniel Harty <dan.john.harty@gmail.com>

--- setup.sh
+++ setup.sh
@@ -22,16 +22,12 @@ mkdir -p files
 # Copy the default tdp_vars
 [[ -d inventory/tdp_vars ]] || cp -r ansible_roles/collections/ansible_collections/tosit/tdp/tdp_vars_defaults inventory/tdp_vars
 
+# Read the TDP releases from file
+tdp_release_uris=$(sed -E '/^[[:blank:]]*(#|$)/d; s/#.*//' $PWD/tdp-release-uris)
+
 # Fetch the TDP .tar.gz releases
-wget https://github.com/TOSIT-FR/hadoop/releases/download/hadoop-project-dist-3.1.1-TDP-0.1.0-SNAPSHOT/hadoop-3.1.1-TDP-0.1.0-SNAPSHOT.tar.gz
-wget https://github.com/TOSIT-FR/hive/releases/download/apache-hive-metastore-3.1.3-TDP-0.1.0-SNAPSHOT/apache-hive-3.1.3-TDP-0.1.0-SNAPSHOT-bin.tar.gz
-wget https://github.com/TOSIT-FR/tez/releases/download/tez-0.9.1-TDP-0.1.0-SNAPSHOT/tez-0.9.1-TDP-0.1.0-SNAPSHOT.tar.gz
-wget https://github.com/TOSIT-FR/spark/releases/download/spark-2.3.5-TDP-0.1.0-SNAPSHOT/spark-2.3.5-TDP-0.1.0-SNAPSHOT-bin-tdp.tgz
-wget https://github.com/TOSIT-FR/ranger/releases/download/ranger-2.0.1-TDP-0.1.0-SNAPSHOT/ranger-2.0.1-TDP-0.1.0-SNAPSHOT-admin.tar.gz
-wget https://github.com/TOSIT-FR/ranger/releases/download/ranger-2.0.1-TDP-0.1.0-SNAPSHOT/ranger-2.0.1-TDP-0.1.0-SNAPSHOT-hdfs-plugin.tar.gz
-wget https://github.com/TOSIT-FR/ranger/releases/download/ranger-2.0.1-TDP-0.1.0-SNAPSHOT/ranger-2.0.1-TDP-0.1.0-SNAPSHOT-hive-plugin.tar.gz
-wget https://github.com/TOSIT-FR/ranger/releases/download/ranger-2.0.1-TDP-0.1.0-SNAPSHOT/ranger-2.0.1-TDP-0.1.0-SNAPSHOT-yarn-plugin.tar.gz
-wget https://github.com/TOSIT-FR/ranger/releases/download/ranger-2.0.1-TDP-0.1.0-SNAPSHOT/ranger-2.0.1-TDP-0.1.0-SNAPSHOT-hbase-plugin.tar.gz
-wget https://github.com/TOSIT-FR/ranger/releases/download/ranger-2.0.1-TDP-0.1.0-SNAPSHOT/ranger-2.0.1-TDP-0.1.0-SNAPSHOT-usersync.tar.gz
-wget https://github.com/TOSIT-IO/phoenix/releases/download/phoenix-hbase-2.1-5.1.3-TDP-0.1.0-SNAPSHOT/phoenix-hbase-2.1-5.1.3-TDP-0.1.0-SNAPSHOT-bin.tar.gz
-wget https://github.com/TOSIT-IO/phoenix-queryserver/releases/download/phoenix-queryserver-6.0.0-TDP-0.1.0-SNAPSHOT/phoenix-queryserver-6.0.0-TDP-0.1.0-SNAPSHOT-bin.tar.gz
+for tdp_release_uri in $tdp_release_uris; do
+    release_name=$(basename $tdp_release_uri)
+    # Fetch the TDP .tar.gz releases
+    [[ -f "$PWD/files/$release_name" ]] || wget $tdp_release_uri -nc -nd -O $PWD/files/$release_name
+done
