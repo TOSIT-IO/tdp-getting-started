ca3e3e7e9adca0f635384f4637a7a4028bdf5d1b
Gauthier Leonard <gauthier@adaltas.com>
2022-04-25 14:38:49 +0200

    fix: correct tdp_extra collection dir

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,18 +9,19 @@
 root_dir="$(dirname "$0")/.."
 
 TDP_ROLES_PATH="$root_dir/ansible_roles/collections/ansible_collections/tosit/tdp"
-TDP_ROLES_EXTRA_PATH="$root_dir/ansible_roles/collections/ansible_collections/tosit/tdp-extra"
+TDP_ROLES_EXTRA_PATH="$root_dir/ansible_roles/collections/ansible_collections/tosit/tdp_extra"
 
 # Create directories
 mkdir -p logs
 mkdir -p files
 
-# Clone ansible-tdp-roles repository (doesn't fail iof not known host)
-[[ -d "$TDP_ROLES_PATH" ]] || git clone -o StrictHostKeyChecking=no git@github.com:TOSIT-IO/tdp-collection.git "$TDP_ROLES_PATH"
-[[ -d "$TDP_ROLES_EXTRA_PATH" ]] || git clone -o StrictHostKeyChecking=no git@github.com:TOSIT-IO/tdp-collection-extras.git "$TDP_ROLES_EXTRA_PATH"
+# Clone ansible-tdp-roles repository (doesn't fail if not known host)
+[[ -d "$TDP_ROLES_PATH" ]] || git clone https://github.com/TOSIT-IO/tdp-collection.git "$TDP_ROLES_PATH"
+[[ -d "$TDP_ROLES_EXTRA_PATH" ]] || git clone https://github.com/TOSIT-IO/tdp-collection-extras.git "$TDP_ROLES_EXTRA_PATH"
 
 # Quick fix for file lookup related to the Hadoop role refactor (https://github.com/TOSIT-FR/ansible-tdp-roles/pull/57)
 [[ -d "$TDP_ROLES_PATH/playbooks/files" ]] || ln -s "$(realpath "$root_dir")/files" "$TDP_ROLES_PATH/playbooks"
+[[ -d "$TDP_ROLES_EXTRA_PATH/playbooks/files" ]] || ln -s "$(realpath "$root_dir")/files" "$TDP_ROLES_EXTRA_PATH/playbooks"
 
 # Copy the default tdp_vars
 [[ -d inventory/tdp_vars ]] || cp -r ansible_roles/collections/ansible_collections/tosit/tdp/tdp_vars_defaults inventory/tdp_vars
