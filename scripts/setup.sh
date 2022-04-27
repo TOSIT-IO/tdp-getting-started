2ecb07d9087a77136e3584060341385748683be5
Gauthier Leonard <gauthier@adaltas.com>
2022-04-27 10:44:40 +0200

    feat: support tdp_extra vars_defaults

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,35 +5,37 @@
 # virtual TDP cluster using the TDP-getting-started repo
 ###
 
+# tdp-getting-started root dir
+rel_root_dir="$(dirname "$0")/.."
+abs_root_dir="$(realpath "$rel_root_dir")"
+
 # tdp-collection
 TDP_COLLECTION_URL=https://github.com/TOSIT-IO/tdp-collection
-TDP_ROLES_PATH=ansible_roles/collections/ansible_collections/tosit/tdp
+TDP_ROLES_PATH="$abs_root_dir/ansible_roles/collections/ansible_collections/tosit/tdp"
 TDP_COLLECTION_COMMIT=86f2d3f42df18a5ac07cc25e847ddaf3082be6d4
 
 # tdp-collection-extras
 TDP_COLLECTION_EXTRAS_URL=https://github.com/TOSIT-IO/tdp-collection-extras
-TDP_ROLES_EXTRA_PATH=ansible_roles/collections/ansible_collections/tosit/tdp_extra
+TDP_ROLES_EXTRA_PATH="$abs_root_dir/ansible_roles/collections/ansible_collections/tosit/tdp_extra"
 TDP_COLLECTION_EXTRAS_COMMIT=1035ca7f3f67275140cd15478d043b543679ec30
-
-root_dir="$(dirname "$0")/.." # project root dir
-EXPANDED_TDP_ROLES_PATH="$(realpath "$root_dir")/$TDP_ROLES_PATH"
-EXPANDED_TDP_ROLES_EXTRA_PATH="$(realpath "$root_dir")/$TDP_ROLES_EXTRA_PATH"
 
 # Create directories
 mkdir -p logs
 mkdir -p files
 
-# Clone ansible-tdp-roles repository (doesn't fail iof not known host)
-[[ -d "$EXPANDED_TDP_ROLES_PATH" ]] || "$(realpath "$root_dir")/scripts/git-commit-download.sh" "$EXPANDED_TDP_ROLES_PATH" "$TDP_COLLECTION_URL" "$TDP_COLLECTION_COMMIT"
-[[ -d "$TDP_ROLES_EXTRA_PATH" ]] || "$(realpath "$root_dir")/scripts/git-commit-download.sh" "$EXPANDED_TDP_ROLES_EXTRA_PATH" "$TDP_COLLECTION_EXTRAS_URL" "$TDP_COLLECTION_EXTRAS_COMMIT"
+# Clone ansible-tdp-roles repository (doesn't fail if not known host)
+[[ -d "$TDP_ROLES_PATH" ]] || "$abs_root_dir/scripts/git-commit-download.sh" "$TDP_ROLES_PATH" "$TDP_COLLECTION_URL" "$TDP_COLLECTION_COMMIT"
+[[ -d "$TDP_ROLES_EXTRA_PATH" ]] || "$abs_root_dir/scripts/git-commit-download.sh" "$TDP_ROLES_EXTRA_PATH" "$TDP_COLLECTION_EXTRAS_URL" "$TDP_COLLECTION_EXTRAS_COMMIT"
 
 # Quick fix for file lookup related to the Hadoop role refactor (https://github.com/TOSIT-FR/ansible-tdp-roles/pull/57)
-[[ -d $EXPANDED_TDP_ROLES_PATH/playbooks/files ]] || ln -s "$(realpath "$root_dir")/files" "$EXPANDED_TDP_ROLES_PATH/playbooks"
-[[ -d $EXPANDED_TDP_ROLES_EXTRA_PATH/playbooks/files ]] || ln -s "$(realpath "$root_dir")/files" "$EXPANDED_TDP_ROLES_EXTRA_PATH/playbooks"
+[[ -d "$TDP_ROLES_PATH/playbooks/files" ]] || ln -s "$abs_root_dir/files" "$TDP_ROLES_PATH/playbooks"
+[[ -d "$TDP_ROLES_EXTRA_PATH/playbooks/files" ]] || ln -s "$abs_root_dir/files" "$TDP_ROLES_EXTRA_PATH/playbooks"
 
-# Copy the default tdp_vars
-[[ -d "$(realpath "$root_dir")/inventory/tdp_vars" ]] || cp -r "$EXPANDED_TDP_ROLES_PATH/tdp_vars_defaults" "$(realpath "$root_dir")/inventory/tdp_vars"
+# Copy the default tdp_vars from tdp-collection and tdp-collection-extras
+mkdir -p "$abs_root_dir/inventory/tdp_vars"
+[[ -d "$TDP_ROLES_PATH/tdp_vars_defaults" ]] && cp -rf "$TDP_ROLES_PATH/tdp_vars_defaults/"* "$abs_root_dir/inventory/tdp_vars"
+[[ -d "$TDP_ROLES_EXTRA_PATH/tdp_extra_vars_defaults" ]] && cp -rf "$TDP_ROLES_EXTRA_PATH/tdp_extra_vars_defaults/"* "$abs_root_dir/inventory/tdp_vars"
 
 # Download tdp release binaries
-tdp_releases="$(realpath "$root_dir")/scripts/tdp-release-uris.txt"
-wget -i "$tdp_releases" -P "$(realpath "$root_dir")/files"
+tdp_releases="$abs_root_dir/scripts/tdp-release-uris.txt"
+wget -i "$tdp_releases" -P "$abs_root_dir/files"
