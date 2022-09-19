cf47ea47ab554418bf623feb6db8646b229e223a
Romain Pignolet <romain.pignolet@dgfip.finances.gouv.fr>
2022-09-19 19:00:58 +0200

    feat: add tdp-lib for deploying TDP

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,8 @@ readonly PYTHON_BIN=${PYTHON_BIN:-python3}
 readonly PYTHON_VENV=${PYTHON_VENV:-venv}
 readonly TDP_COLLECTION_PATH="ansible_roles/collections/ansible_collections/tosit/tdp"
 readonly TDP_COLLECTION_EXTRAS_PATH="ansible_roles/collections/ansible_collections/tosit/tdp_extra"
+readonly SQLITE_DB_PATH=${SQLITE_DB_PATH:-sqlite.db}
+readonly TDP_DATABASE_DSN=${TDP_DATABASE_DSN:-sqlite:///$SQLITE_DB_PATH}
 
 CLEAN="false"
 declare -a FEATURES
@@ -71,18 +73,6 @@ validate_features() {
 
 create_directories() {
   mkdir -p logs files inventory/topologies
-}
-
-setup_python_venv() {
-  [[ -d "$PYTHON_VENV" ]] && return 0
-  echo "Setup python venv with '${PYTHON_BIN}' to '${PYTHON_VENV}'"
-  "$PYTHON_BIN" -m venv "$PYTHON_VENV"
-  (
-    source "${PYTHON_VENV}/bin/activate"
-    pip install -U pip
-    pip install -r requirements.txt
-  )
-  return 0
 }
 
 git_submodule_setup() {
@@ -150,25 +140,64 @@ setup_submodule_vagrant() {
   create_symlink_if_needed "../.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory" "inventory/hosts.ini"
 }
 
-setup_tdp_vars() {
-  local tdp_vars="inventory/tdp_vars"
+setup_submodule_tdp_lib() {
+  git_submodule_setup "tdp-lib"
+  local collection_path="$TDP_COLLECTION_PATH"
+  local env_path=".env"
+
+  for feature in "${FEATURES[@]}"; do
+    case "$feature" in
+    extras) collection_path="${collection_path}:${TDP_COLLECTION_EXTRAS_PATH}" ;;
+    esac
+  done
+
   if [[ "$CLEAN" == "true" ]]; then
-    echo "Remove ${tdp_vars}"
-    rm -rf "$tdp_vars"
+    echo "Remove ${env_path}"
+    rm -rf "$env_path"
   fi
-  if [[ -e "$tdp_vars" ]]; then
-    echo "File ${tdp_vars} exists, nothing to do"
+  if [[ -e "${env_path}" ]]; then
+    echo "File ${env_path} exists, nothing to do"
     return 0
   fi
-  mkdir "$tdp_vars"
-  local tdp_vars_defaults_to_copy=(
-    "${TDP_COLLECTION_PATH}/tdp_vars_defaults"
-    "${TDP_COLLECTION_EXTRAS_PATH}/tdp_vars_defaults"
+  cat <<EOF > "$env_path"
+export TDP_COLLECTION_PATH=${collection_path}
+export TDP_RUN_DIRECTORY=.
+export TDP_DATABASE_DSN=${TDP_DATABASE_DSN}
+export TDP_VARS=inventory/tdp_vars
+EOF
+}
+
+setup_python_venv() {
+  if [[ ! -d "$PYTHON_VENV" ]]; then
+    echo "Create python venv with '${PYTHON_BIN}' to '${PYTHON_VENV}' and update pip to latest version"
+    "$PYTHON_BIN" -m venv "$PYTHON_VENV"
+    (
+      source "${PYTHON_VENV}/bin/activate"
+      pip install -U pip
+    )
+  else
+    echo "Python venv '${PYTHON_VENV}' already exists, nothing to do"
+  fi
+  echo "Install python dependencies"
+  (
+    source "${PYTHON_VENV}/bin/activate"
+    pip install -r requirements.txt
+    pip install --editable tdp-lib[visualization]
   )
-  for tdp_vars_defaults in "${tdp_vars_defaults_to_copy[@]}"; do
-    [[ -d "$tdp_vars_defaults" ]] && cp -rf "${tdp_vars_defaults}/"* "$tdp_vars"
-  done
-  echo "tdp_vars_defaults copied to '${tdp_vars}'"
+  return 0
+}
+
+init_tdp_lib() {
+  local tdp_vars="inventory/tdp_vars"
+  if [[ "$CLEAN" == "true" ]]; then
+    echo "Remove '${tdp_vars}' and '${SQLITE_DB_PATH}'"
+    rm -rf "$tdp_vars" "$SQLITE_DB_PATH"
+  fi
+  echo "tdp-lib init"
+  (
+    source "${PYTHON_VENV}/bin/activate"
+    tdp init
+  )
   return 0
 }
 
@@ -181,7 +210,6 @@ main() {
   [[ "$HELP" == "true" ]] && { print_help; exit 0; }
   validate_features
   create_directories
-  setup_python_venv
 
   setup_submodule_tdp
 
@@ -193,7 +221,9 @@ main() {
     esac
   done
 
-  setup_tdp_vars
+  setup_submodule_tdp_lib
+  setup_python_venv
+  init_tdp_lib
   download_tdp_binaries
 }
 
